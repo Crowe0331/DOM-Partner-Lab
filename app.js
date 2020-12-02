814feddfe0be53d8f3449f081a61ce8dd91dc207
jeremyking1996 <73255406+jeremyking1996@users.noreply.github.com>
2020-12-02 14:21:25 -0500

    Part two

--- app.js
+++ app.js
@@ -1,45 +1,109 @@
 
-// Step one
-let teamList= document.getElementsByClassName("list")
+// // Step one
+// let teamList= document.getElementsByClassName("list")
 
-let listNames= ['Randy', 'Tim', 'Andrew', 'Sandy', 'Cindy']
+// let listNames= ['Randy', 'Tim', 'Andrew', 'Sandy', 'Cindy']
 
-for(i = 0; i < listNames.length; i++){
-    teamList[i].textContent = listNames[i];
+// for(i = 0; i < listNames.length; i++){
+//     teamList[i].textContent = listNames[i];
+// }
+
+// // Step two
+// let imgSize = prompt("Enter small or large");
+// let philly= document.getElementById("philly");
+
+// if(imgSize == "small"){
+//     philly.setAttribute("width", "250px");
+//     philly.setAttribute("height","400px");
+// } else if(imgSize == "large"){
+//     philly.setAttribute("width", "500px");
+//     philly.setAttribute("height", "800px");
+// }else{
+//     prompt("Try again!");
+// }
+
+// // Step three
+// let userColor= prompt("Insert a background color. Choose from red, blue or green")
+
+// if(userColor== "red"){
+//     document.body.style.backgroundColor = "rgba(255, 0, 0)";
+// }else if(userColor== "blue"){
+//     document.body.style.backgroundColor = "rgba(34, 167, 240, 1)";
+// }else if(userColor== "green"){
+//     document.body.style.backgroundColor = "rgba(22, 160, 133, 1)";
+// }else{
+//     prompt("Try again!")
+// }
+
+// // Step 4
+
+// for(i = 0; i < 5; i++){
+//     let team1= prompt("Team member name");
+//     teamList[i].textContent = team1;
+// };
+
+
+// Part 2
+
+// Step 1
+let userName = prompt("Enter your username");
+
+let header= document.createElement("h1");
+let title= document.createTextNode(userName + "'s resume!")
+header.appendChild(title);
+document.body.appendChild(header);
+
+// Step 2
+let skillArr = ["SKill1", "Skill2", "skill3", "skill4"];
+
+let userChoice= prompt("Do you want the list ordered or unordered?");
+
+
+
+let list;
+  
+if(userChoice == "ordered"){    
+    list= document.createElement("ol");
+}else if(userChoice== "unordered"){
+    list= document.createElement("ul");
+}else (prompt("try again!"));
+
+for(let i = 0; i < skillArr.length; i++){
+    document.body.appendChild(list)
+    let skills = document.createElement('li')
+    let text = document.createTextNode(skillArr[i]);
+    skills.appendChild(text);
+    list.appendChild(skills);
 }
 
-// Step two
-let imgSize = prompt("Enter small or large");
-let philly= document.getElementById("philly");
 
-if(imgSize == "small"){
-    philly.setAttribute("width", "250px");
-    philly.setAttribute("height","400px");
-} else if(imgSize == "large"){
-    philly.setAttribute("width", "500px");
-    philly.setAttribute("height", "800px");
-}else{
-    prompt("Try again!");
-}
 
-// Step three
-let userColor= prompt("Insert a background color. Choose from red, blue or green")
 
-if(userColor== "red"){
-    document.body.style.backgroundColor = "rgba(255, 0, 0)";
-}else if(userColor== "blue"){
-    document.body.style.backgroundColor = "rgba(34, 167, 240, 1)";
-}else if(userColor== "green"){
-    document.body.style.backgroundColor = "rgba(22, 160, 133, 1)";
-}else{
-    prompt("Try again!")
-}
 
-// Step 4
 
-for(i = 0; i < 5; i++){
-    let team1= prompt("Team member name");
-    teamList[i].textContent = team1;
-};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
